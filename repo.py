c7a8888d699ca32257a0c0763b88f41e95da7b49
Farhad Arziev <farhadarziev8@gmail.com>
2023-11-29 19:59:02 +0600

    first

--- repo.py
+++ repo.py
@@ -1,1 +1,16 @@
-print('repo')
+total_weight = 713
+box_weights = [0, 0, 0]
+box_kilometr = [0, 0, 0]
+
+for i in range(3):
+    box_kilometr[i] = int(input())
+    box_weights[i] = int(input())
+
+while sum(box_weights) != total_weight:
+    print("the cargo is not found")
+
+    for i in range(3):
+        box_kilometr[i] = int(input())
+        box_weights[i] = int(input())
+
+print("The cargo is found")
